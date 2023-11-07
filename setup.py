5fbe726fa6a13fd3a6f5336391c2c64819964a60
Jörg Breitbart <jerch@rockborn.de>
2023-11-07 12:49:13 +0100

    new release

--- setup.py
+++ setup.py
@@ -1,22 +1,36 @@
 from setuptools import setup, find_packages
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except(IOError, ImportError, RuntimeError):
-    long_description = ''
+with open('README.md', 'r') as f:
+    long_description = f.read()
 
 setup(
     name='django-hidefield',
     packages=find_packages(exclude=['example']),
     include_package_data=True,
-    version='0.1.2',
+    version='0.2.0',
     description='hide fields in django admin',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Joerg Breitbart',
     author_email='j.breitbart@netzkolchose.de',
     url='https://github.com/jerch/django-hidefield',
-    download_url='https://github.com/jerch/django-hidefield/archive/0.1.2.tar.gz',
+    download_url='https://github.com/jerch/django-hidefield/archive/0.2.0.tar.gz',
     keywords=['django', 'widget', 'admin', 'field', 'hide'],
-    classifiers=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Database',
+        'Topic :: Database :: Front-Ends',
+        'Topic :: Software Development :: Libraries',
+        'Framework :: Django',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.2',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
+    ],
 )
